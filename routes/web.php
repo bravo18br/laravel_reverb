e6ec6d73ea5929f2783581f9bbfab3b38550c9b7
bravo18br <bravo18br@gmail.com>
2024-10-04 11:30:31 -0300

    04/10

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\MessageTestNotification;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('send-notification', function () {
+    $message['status']=request()->query('status','success');
+    $message['body']='Mensagem body padrão';
+
+    $u = User::first();
+    $u->notify(new MessageTestNotification($message));
+
+    return 'Notificação enviada';
+});
+
+Route::get('login/{id}', function (int $id) {
+    // Verifica se o método está disponível e faz login
+    if (method_exists(Auth::guard(), 'loginUsingId')) {
+        Auth::loginUsingId($id);
+        return 'User logged in!';
+    } else {
+        return 'The loginUsingId method is not available for the current guard.';
+    }
 });
